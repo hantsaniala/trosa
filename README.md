9251c73ba4613b61531c298cdb3b9cf7c38249a7
Hantsaniala Eléo <hantsaniala@gmail.com>
2020-06-03 11:16:22 +0300

    Init projet

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trosa
+
+A little loaning management.
+
+&copy; Copyright Hantsaniala 2020
---
+++ android/app/src/main/kotlin/mg/hantsaniala/trosa/MainActivity.kt
@@ -0,0 +1,6 @@
+package mg.hantsaniala.trosa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
