d76af07deda699bad2d73a196285863ecdad1fb1
Hantsaniala Eléo <hantsaniala@gmail.com>
2020-08-10 21:52:19 +0300

    Adding TrosaCard widget

---
+++ lib/screens/trosa/components/trosa_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class TrosaCard extends StatelessWidget {
+  final bool isInflow;
+  final String amount;
+  final String owner;
+  final String dueDate;
+  final String date;
+  final String note;
+  const TrosaCard({
+    Key key,
+    this.data,
+    this.amount,
+    this.owner,
+    this.dueDate,
+    this.date,
+    this.isInflow,
+    this.note,
+  }) : super(key: key);
+
+  final List data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(
+              isInflow ? Icons.arrow_downward : Icons.arrow_upward,
+              color: isInflow ? Colors.green : Colors.red,
+              size: 40,
+            ),
+            title: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Ar ' + amount.toString(),
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(' — ' + owner,
+                    style: Theme.of(context).textTheme.bodyText1,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1),
+              ],
+            ),
+            subtitle: Text(
+              note,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  'Haverina ny ' + dueDate.toString(),
+                  style: TextStyle(fontSize: 15),
+                ),
+                Text(
+                  'Natao ny ' + date.toString(),
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
